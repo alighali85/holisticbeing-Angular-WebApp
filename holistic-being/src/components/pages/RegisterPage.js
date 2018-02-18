d602851ce52427c9fc94a97be6a96591d7092d65
Ali <ali@alighali.com>
2018-02-18 22:26:06 +0100

    add new router component

---
+++ holistic-being/src/components/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const RegisterPage = ()=> (
+    <div>
+        <div class="ui row ">
+        Register New User component
+        </div>
+
+    </div>
+)
+
+export default RegisterPage
